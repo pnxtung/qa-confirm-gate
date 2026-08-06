e7ee42727aa3cbee2d2d2d2f1d468d8321fb12d3
pnxtung <pnxtung@gmail.com>
2026-08-07 04:46:36 +0700

    refactor: Delete root render.yaml as Render Dashboard now targets PRODUCT/Render GDrive/source directly

--- Start_Cloudflare_Tunnel.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title QA Confirm Gate - Cloudflare Public Tunnel
-echo ==================================================
-echo STARTING INTERNET TUNNEL VIA CLOUDFLARE
-echo Make sure Start_Server.bat is running on http://localhost:8000!
-echo ==================================================
-echo.
-"%~dp0tools\cloudflared.exe" tunnel --url http://localhost:8000
-pause
